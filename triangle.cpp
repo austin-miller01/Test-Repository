8985fc1904615463bdf59ede6d339de13c0dfb5d
Austin Miller <ajm19x@my.fsu.edu>
2021-01-12 19:45:29 -0500

    adds some random code to repository for testing

---
+++ triangle.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Book{
+    int isbn;
+    string name;
+
+    public:
+        void setname(string newName){ name = newName;};
+        void print(){cout << name << " " << isbn << endl;};
+        void setISBN(int num){isbn = num;};
+
+};
+
+
+int main(){
+    Book b;
+    b.setname("hello world");
+    b.setISBN(24);
+    b.print();
+    return 0;
+
+}   
